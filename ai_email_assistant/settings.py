6aadf39abbebc23451434a5ffa336ae3f0940c25
krazxx <anshul.negi2k5@gmail.com>
2026-01-29 18:44:09 +0530

    Fix static files for production

--- ai_email_assistant/settings.py
+++ ai_email_assistant/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 LOGIN_REDIRECT_URL = '/'
