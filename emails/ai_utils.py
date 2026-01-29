8a58b193fb5619abd538a060dd10c1530b69e774
krazxx <anshul.negi2k5@gmail.com>
2026-01-29 21:01:12 +0530

    Fix Gmail OAuth functions

--- emails/ai_utils.py
+++ emails/ai_utils.py
@@ -1,6 +1,48 @@
 import requests
 import json
 import re
+import os
+import json
+from google_auth_oauthlib.flow import Flow
+from google.oauth2.credentials import Credentials
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+def start_gmail_auth(request):
+    flow = Flow.from_client_secrets_file(
+        'credentials.json',
+        scopes=SCOPES,
+        redirect_uri=request.build_absolute_uri('/emails/oauth2callback/')
+    )
+
+    auth_url, state = flow.authorization_url(
+        access_type='offline',
+        prompt='consent'
+    )
+
+    request.session['state'] = state
+    return auth_url
+
+
+def save_user_token(request, user):
+    state = request.session['state']
+
+    flow = Flow.from_client_secrets_file(
+        'credentials.json',
+        scopes=SCOPES,
+        state=state,
+        redirect_uri=request.build_absolute_uri('/emails/oauth2callback/')
+    )
+
+    flow.fetch_token(authorization_response=request.build_absolute_uri())
+
+    creds = flow.credentials
+
+    token_path = f"token_{user.id}.json"
+    with open(token_path, 'w') as token:
+        token.write(creds.to_json())
+
 
 OLLAMA_URL = " https://vanquishable-liplike-rosina.ngrok-free.dev"
 MODEL = "llama3"
